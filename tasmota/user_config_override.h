a0db0ff33a92948b1f7c28fb7e9919290590c8bb
lerlink001-cmd <lerlink001@gmail.com>
2026-03-30 11:04:21 +0800

    Update user_config_override.h

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -1,26 +1,15 @@
 #ifndef _USER_CONFIG_OVERRIDE_H_
 #define _USER_CONFIG_OVERRIDE_H_
 
-// 启用 ESP32-C3
 #define USE_ESP32C3_DEV
-
-// 必须开启：允许配置 GPIO 和模板
 #define USE_MODULES
 #define USE_TEMPLATE
-#define USE_GPIO
-
-// 你的硬件自动配置（100%对应你的引脚）
-#define MODULE            USER_MODULE
-#define USER_TEMPLATE     "{\"NAME\":\"ESP32-C3-HLW8032\",\"GPIO\":[0,0,384,0,32,33,0,0,224,0,0,0,0,0,0,0,0,0,161,160,1056,0],\"FLAG\":0,\"BASE\":0}"
-
-// 开启功率计量
 #define USE_HLW8032
-#define USE_ENERGY_MONITOR
 
-// 自动参数
-#define SET_OPTION1  1
-#define SET_OPTION13 1
-#define SET_OPTION30 1
-#define SET_OPTION63 1
+#undef MODULE
+#define MODULE USER_MODULE
+
+#undef USER_TEMPLATE
+#define USER_TEMPLATE "{\"NAME\":\"ESP32-C3-HLW8032\",\"GPIO\":[0,0,384,0,32,33,0,0,224,0,0,0,0,0,0,0,0,0,161,160,1056,0],\"FLAG":0,"BASE":1}"
 
 #endif
