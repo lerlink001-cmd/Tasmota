cdedde53d8837ea9ebc15630915c1fb1383d211a
lerlink001-cmd <lerlink001@gmail.com>
2026-04-09 09:51:10 +0800

    Update user_config_override.h

--- user_config_override.h
+++ user_config_override.h
@@ -2,17 +2,17 @@
 #ifndef _USER_CONFIG_OVERRIDE_H_
 #define _USER_CONFIG_OVERRIDE_H_
 
-// 开启红外功能
+// 启用红外
 #define USE_IR_REMOTE
 #define USE_IR_RECEIVE
 
-// 强制模块 = 自定义模式（永不自动变 Sonoff/Teckin）
+// 强制模块 = 自定义模式（必须 0）
 #undef MODULE
 #define MODULE 0
 
-// 你的最终模板（命令成功的那个一模一样）
-#undef TEMPLATE
-#define TEMPLATE "{\"NAME\":\"Lerlink_ESP8266_IR\",\"GPIO\":[32,0,0,0,1056,1088,0,0,0,576,0,0,0,0],\"FLAG\":0,\"BASE\":1}"
+// 🔥 【关键错误】你之前一直用 TEMPLATE，正确是 DEFAULT_TEMPLATE 🔥
+#undef DEFAULT_TEMPLATE
+#define DEFAULT_TEMPLATE "{\"NAME\":\"Lerlink_ESP8266_IR\",\"GPIO\":[32,0,0,0,1056,1088,0,0,0,576,0,0,0,0],\"FLAG\":0,\"BASE\":1}"
 
 // 关闭硬件自动检测
 #undef TEMPLATE_AUTODETECT
