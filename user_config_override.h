433542717f37b5a9d5c0ab67264c876096cd99dc
lerlink001-cmd <lerlink001@gmail.com>
2026-04-09 09:08:38 +0800

    Update user_config_override.h

--- user_config_override.h
+++ user_config_override.h
@@ -1,20 +1,22 @@
 
+
 #ifndef _USER_CONFIG_OVERRIDE_H_
 #define _USER_CONFIG_OVERRIDE_H_
 
-// 强制开启红外（标准分支也能用）
+// 启用红外功能
 #define USE_IR_REMOTE
 #define USE_IR_RECEIVE
 
-// 强制自定义模板（永远不被覆盖）
-#undef TEMPLATE_GENERIC
-#define TEMPLATE_GENERIC "{\"NAME\":\"Lerlink_ESP8266_IR\",\"GPIO\":[32,0,0,0,1056,1088,0,0,0,576,0,0,0,0],\"FLAG\":0,\"BASE\":1}"
+// 强制模块 = 0（自定义模式，永不自动变成 Sonoff/Teckin）
+#undef  MODULE
+#define MODULE 0
 
-// 强制通用模块 = 不会变 Sonoff / Teckin
-#undef MODULE
-#define MODULE 1
+// 你的最终模板（和你命令里的一模一样）
+#undef  TEMPLATE
+#define TEMPLATE "{\"NAME\":\"Lerlink_ESP8266_IR\",\"GPIO\":[32,0,0,0,1056,1088,0,0,0,576,0,0,0,0],\"FLAG\":0,\"BASE\":1}"
 
-#undef CFG_HOLDER
+// 防止配置被重置
+#undef  CFG_HOLDER
 #define CFG_HOLDER 999889
 
 #endif
